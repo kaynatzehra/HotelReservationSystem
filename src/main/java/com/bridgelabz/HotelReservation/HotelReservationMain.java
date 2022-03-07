e391c0ab66f1c9fc46959b5dca2575b928133175
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-03-07 16:47:39 +0530

    Welcome To The Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/HotelReservation/HotelReservationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.HotelReservation;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to the hotel reservation system");
+    }
+}
